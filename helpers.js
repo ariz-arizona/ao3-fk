33ea8ae53c40caef8bd4ee849fade81ef36b7ccb
arizona <ar.ariz.arizona@gmail.com>
2022-01-13 22:04:50 +0300

    заменила jsdom на node-html-parser

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+const fetch = require('cross-fetch');
+
 const getRandomInt = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -28,4 +30,22 @@ const closeWindow = (dom) => {
     }
 }
 
-module.exports = { getRandomInt, makeQueryString, getSearchParametres, closeWindow }
+const loadPage = async (url) => {
+    // console.log(url)
+    if (!url) {
+        return false
+    }
+    try {
+        const res = await fetch(url);
+
+        if (res.status >= 400) {
+            throw new Error("Bad response from server");
+        }
+
+        return await res.text();
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+module.exports = { getRandomInt, makeQueryString, getSearchParametres, closeWindow, loadPage }
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
-const jsdom = require("jsdom");
+const HTMLParser = require('node-html-parser');
 
-const { getRandomInt, makeQueryString, getSearchParametres, closeWindow } = require('./helpers');
+const { getRandomInt, makeQueryString, getSearchParametres, closeWindow, loadPage } = require('./helpers');
 const { BOT_TOKEN } = process.env;
-const { JSDOM } = jsdom;
 
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
@@ -33,71 +32,71 @@ bot.onText(/\/cit/, async (msg) => {
             techMsgId = msg.message_id;
         });
 
-        await JSDOM.fromURL(`${worksUrl}${makeQueryString(queryAttrs)}`).then(dom => {
-            const lastPageUrl = dom.window.document.querySelector('.pagination li:nth-last-child(2) a').href;
+        let content;
+        let dom;
 
-            closeWindow(dom);
+        content = await loadPage(`${worksUrl}${makeQueryString(queryAttrs)}`);
+        dom = HTMLParser.parse(content);
 
-            const searchParams = getSearchParametres(lastPageUrl);
-            const randomPage = getRandomInt(1, searchParams.page);
+        const lastPageUrl = dom.querySelector('.pagination li:nth-last-child(2) a').getAttribute('href');
+        const searchParams = getSearchParametres(lastPageUrl);
+        const randomPage = getRandomInt(1, searchParams.page);
 
-            pageQuery = searchParams;
-            pageQuery.page = randomPage;
+        pageQuery = searchParams;
+        pageQuery.page = randomPage;
 
-            bot.editMessageText('Выбрал случайную страницу', { chat_id: chatId, message_id: techMsgId });
-        });
+        bot.editMessageText('Выбрал случайную страницу', { chat_id: chatId, message_id: techMsgId });
 
-        let randomWorkUrl;
         const randomPageUrl = `${worksUrl}${makeQueryString(pageQuery)}`;
 
-        await JSDOM.fromURL(`${randomPageUrl}`).then(dom => {
-            const worksCount = dom.window.document.querySelectorAll('.work > li').length - 1;
-            const randomWork = getRandomInt(0, worksCount);
-            randomWorkUrl = dom.window.document.querySelectorAll('.work > li')[randomWork].querySelector('.heading > a').href;
-            closeWindow(dom);
+        content = await loadPage(`${randomPageUrl}`);
+        dom = HTMLParser.parse(content);
 
-            bot.editMessageText('Выбрал случайную работу', { chat_id: chatId, message_id: techMsgId });
-            console.log(`Для чат айди ${chatId} выбрана работа ${randomWorkUrl}`);
-        });
+        const worksCount = dom.querySelectorAll('.work > li').length - 1;
+        const randomWork = getRandomInt(0, worksCount);
+        const randomWorkUrl = dom.querySelectorAll('.work > li')[randomWork].querySelector('.heading > a').getAttribute('href');
 
-        await JSDOM.fromURL(`${randomWorkUrl}${makeQueryString({ 'view_full_work': 'true', 'view_adult': 'true' })}`).then(dom => {
-            const fandom = dom.window.document.querySelector('dd.fandom.tags').textContent.trim();
-            const title = dom.window.document.querySelector('.title.heading').textContent.trim();
-            const downloadLink = dom.window.document.querySelector('.download > ul > li:nth-child(2) > a').href;
-            const summary = dom.window.document.querySelector('.summary .userstuff') ? dom.window.document.querySelector('.summary .userstuff').textContent.trim() : '';
-            const paragraphs = dom.window.document.querySelectorAll('#chapters .userstuff > p');
-            closeWindow(dom);
+        bot.editMessageText('Выбрал случайную работу', { chat_id: chatId, message_id: techMsgId });
+        console.log(`Для чат айди ${chatId} выбрана работа ${randomWorkUrl}`);
 
-            bot.editMessageText('Ищу случайный абзац', { chat_id: chatId, message_id: techMsgId });
+        content = await loadPage(`${ao3Url}${randomWorkUrl}${makeQueryString({ 'view_full_work': 'true', 'view_adult': 'true' })}`);
+        dom = HTMLParser.parse(content);
 
-            let randomParagraph, randomParagraphText;
-            let i = 0;
-            const paragraphsCount = paragraphs.length - 1;
+        const fandom = dom.querySelector('dd.fandom.tags').textContent.trim();
+        const title = dom.querySelector('.title.heading').textContent.trim();
+        const downloadLink = dom.querySelector('.download > ul > li:nth-child(2) > a').href;
+        const summary = dom.querySelector('.summary .userstuff') ? dom.querySelector('.summary .userstuff').textContent.trim() : '';
+        const paragraphs = dom.querySelectorAll('#chapters .userstuff > p');
 
-            do {
-                randomParagraph = getRandomInt(0, paragraphsCount);
-                randomParagraphText = paragraphs[randomParagraph].textContent.trim().substring(0, 2048);
-                bot.editMessageText(`Ищу случайный абзац ${i + 1} раз`, { chat_id: chatId, message_id: techMsgId });
-                i++;
-            } while (randomParagraphText === '' || i > 5)
+        bot.editMessageText('Ищу случайный абзац', { chat_id: chatId, message_id: techMsgId });
 
-            bot.editMessageText('Все нашел!', { chat_id: chatId, message_id: techMsgId });
-            console.log(`Для чат айди ${chatId} загружена работа ${randomWorkUrl}`);
+        let randomParagraph, randomParagraphText;
+        let i = 0;
+        const paragraphsCount = paragraphs.length - 1;
 
-            bot.sendMessage(
-                chatId,
-                `<b>Случайная работа</b>\n\n<b>Название</b>: ${title}\n\n<b>Фандом</b>: ${fandom}\n\n<b>Ссылка на EPUB</b>: ${downloadLink}\n\n<b>Саммари</b>: ${summary}\n\n<b>Ссылка на документ</b>: ${randomWorkUrl}`,
-                { parse_mode: 'HTML' }
-            ).then(
-                () => {
-                    return bot.sendMessage(
-                        chatId,
-                        `<b>Случайный параграф</b>\n${randomParagraphText}`,
-                        { parse_mode: 'HTML' }
-                    );
-                }
-            );
-        });
+        do {
+            randomParagraph = getRandomInt(0, paragraphsCount);
+            randomParagraphText = paragraphs[randomParagraph].textContent.trim().substring(0, 2048);
+            bot.editMessageText(`Ищу случайный абзац ${i + 1} раз`, { chat_id: chatId, message_id: techMsgId });
+            i++;
+        } while (randomParagraphText === '' || i > 5)
+
+        bot.editMessageText('Все нашел!', { chat_id: chatId, message_id: techMsgId });
+        console.log(`Для чат айди ${chatId} загружена работа ${randomWorkUrl}`);
+
+        bot.sendMessage(
+            chatId,
+            `<b>Случайная работа</b>\n\n<b>Название</b>: ${title}\n\n<b>Фандом</b>: ${fandom}\n\n<b>Ссылка на EPUB</b>: ${downloadLink}\n\n<b>Саммари</b>: ${summary}\n\n<b>Ссылка на документ</b>: ${randomWorkUrl}`,
+            { parse_mode: 'HTML' }
+        ).then(
+            () => {
+                return bot.sendMessage(
+                    chatId,
+                    `<b>Случайный параграф</b>\n${randomParagraphText}`,
+                    { parse_mode: 'HTML' }
+                );
+            }
+        );
 
         if (process.memoryUsage().heapUsed > 200000000) {
             global.gc();
