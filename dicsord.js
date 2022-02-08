c94ed930ec15cb6df2429244222a8c9467343317
arizona <ar.ariz.arizona@gmail.com>
2022-02-08 22:15:12 +0300

    даунгрейд, эмбед

--- dicsord.js
+++ dicsord.js
@@ -1,40 +1,36 @@
-const { Client, Intents, MessageAttachment } = require('discord.js');
+const { Client, Intents } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 // const { REST } = require('@discordjs/rest');
 // const { Routes } = require('discord-api-types/v9');
+// const slash = require('discord-slash-commands-v12');
+
 const { makeWorksUrl, searchWorkPage, getWorkData, getWorkImages, getRandomParagraph, makeWorkAnswer } = require('./functions/func');
 const { ao3Url } = require('./constants');
 
-const { DISCORD_TOKEN, DISCORD_GUILD_ID, DISCORD_CLIENT_ID } = process.env;
+const { DISCORD_TOKEN } = process.env;
+const prefix = '!';
 
-const commands = [
-    new SlashCommandBuilder().setName('random').setDescription('Get random work'),
-]
-    .map(command => command.toJSON());
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const discord = async () => {
-    const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-    client.once('ready', () => {
+    client.on('ready', () => {
         console.log('Ready!');
     });
 
-    client.login(DISCORD_TOKEN);
+    // даунгрейд до 12 версии
+    client.on('message', async (msg) => {
+        if (msg.author.bot) return;
+        if (!msg.guild) return;
+        if (!msg.content.startsWith(prefix)) return;
 
-    // const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN);
+        const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+        const cmd = args.shift().toLowerCase();
 
-    // rest.put(Routes.applicationGuildCommands(DISCORD_CLIENT_ID, DISCORD_GUILD_ID), { body: commands })
-    //     .then(() => console.log('Successfully registered application commands.'))
-    //     .catch(console.error);
+        if (cmd.length === 0) return;
 
-    client.on('interactionCreate', async interaction => {
-        if (!interaction.isCommand()) return;
-
-        const { commandName } = interaction;
-
-        switch (commandName) {
+        switch (cmd) {
             case 'random':
-                await interaction.reply(`Начинаю искать по тегам ${global.additionalTag} и ${global.seasonTag}`);
+                const techMsg = await msg.reply(`Начинаю искать по тегам ${global.additionalTag} и ${global.seasonTag}`);
 
                 const queryAttrs = {
                     // 'work_search%5Bwords_to%5D': 100
@@ -47,13 +43,13 @@ const discord = async () => {
                 const worksUrl = makeWorksUrl(global.seasonTag);
                 const { dom, randomWorkUrl } = await searchWorkPage(worksUrl, queryAttrs);
 
-                await interaction.editReply(`Собираю данные работы`);
+                await techMsg.edit(`Собираю данные работы`);
                 const { fandom, title, downloadLink, summary } = await getWorkData(dom);
 
-                await interaction.editReply(`Ищу случайный абзац`);
-                const randomParagraphText = getRandomParagraph(dom);
+                await techMsg.edit(`Ищу случайный абзац`);
+                const randomParagraphText = getRandomParagraph(dom).slice(0, 900);
 
-                await interaction.editReply(`Ищу картинки`);
+                await techMsg.edit(`Ищу картинки`);
                 const { media, otherLinks } = getWorkImages(dom);
 
                 const images = [];
@@ -63,22 +59,7 @@ const discord = async () => {
                     })
                 })
 
-                const response = [
-                    `**Фандом** ${fandom}`,
-                    `**Название** ${title}`,
-                ];
-
-                if (summary) {
-                    response.push(`\n${summary}`);
-                }
-
-                response.push(`[Ссылка на скачивание](${ao3Url}${downloadLink})`);
-                response.push(`[Ссылка на страницу работы](${ao3Url}${randomWorkUrl})`);
-
-                if (randomParagraphText) {
-                    response.push(`\n**Случайный абзац**\n${randomParagraphText}`)
-                }
-
+                const response = []
                 if (images.length) {
                     response.push(`\nНашел картинки: ${images.join('\n')}`)
                 }
@@ -87,11 +68,50 @@ const discord = async () => {
                     response.push(`\nНашел видео: ${otherLinks.join('\n')}`)
                 }
 
-                await interaction.editReply({ content: response.join('\n') });
+                await techMsg.edit(`Все нашел!`)
+
+                const embed = {
+                    title: title,
+                    fandom: fandom,
+                    url: `${ao3Url}${randomWorkUrl}`,
+                    fields: [
+                        {
+                            name: 'Фандом',
+                            value: fandom,
+                        },
+                        {
+                            name: 'Ссылка для скачивания',
+                            value: `${ao3Url}${downloadLink}`,
+                        },
+                    ],
+                }
+                if (randomParagraphText) embed.fields.push({
+                    name: 'Случайный абзац',
+                    value: randomParagraphText
+                });
+                if (summary) embed.fields.push({
+                    name: 'Саммари',
+                    value: summary,
+                });
+                if (images.length) {
+                    embed.image = { url: images[0] };
+                    embed.fields.push({
+                        name: 'Картинки',
+                        value: images.join('\n'),
+                    });
+                }
+                if (otherLinks.length) embed.fields.push({
+                    name: 'Видео',
+                    value: otherLinks.join('\n'),
+                });
+
+                await techMsg.edit({ embed });
                 break;
             default:
         }
     });
+
+    client.login(DISCORD_TOKEN);
 }
 
 module.exports = discord
