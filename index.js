eff390ffd2811da5e703b5175a5419a23e712288
arizona <ar.ariz.arizona@gmail.com>
2022-02-09 00:28:33 +0300

    discor endpoint

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
-const { InteractionType, InteractionResponseType } = require('discord-interactions');
+const { InteractionType, InteractionResponseType, verifyKey } = require('discord-interactions');
 
-const { fkTagYears, winterFkTag } = require('./constants');
+const { fkTagYears, winterFkTag, ao3Url } = require('./constants');
 const { set, cit, pic, collection, onCallbackQuery } = require('./functions/main');
-const { showError } = require('./functions/func');
+const { showError, makeWorkAnswer, makeWorksUrl, searchWorkPage, getRandomParagraph, getWorkImages, getWorkData } = require('./functions/func');
 
 const { getRandomCit } = require('./dicsord');
 
@@ -40,7 +40,12 @@ bot.on('polling_error', (error) => {
     console.log(error.code);
 });
 
-app.use(express.json());
+app.use(express.json({
+    limit: '5mb',
+    verify: (req, res, buf) => {
+        req.rawBody = buf.toString();
+    },
+}));
 
 app.get('/', async (_req, res) => {
     res.send(`listening on ${CURRENT_HOST}`)
@@ -84,17 +89,88 @@ app.post(`/callback`, async (_req, res) => {
 });
 
 app.post('/discord', async (_req, res) => {
-    const message = _req.body;
+    const signature = _req.headers['x-signature-ed25519'];
+    const timestamp = _req.headers['x-signature-timestamp'];
+    const isValidRequest = verifyKey(
+        _req.rawBody,
+        signature,
+        timestamp,
+        process.env.DISCORD_PUB_KEY
+    );
 
+    if (!isValidRequest) {
+        return res.status(401).send({ error: 'Bad request signature ' });
+    }
+
+    const message = _req.body;
+    console.log(message)
     if (message.type === InteractionType.PING) {
-        res.send({
+        res.status(200).send({
             type: InteractionResponseType.PONG,
         });
-        
     } else if (message.type === InteractionType.APPLICATION_COMMAND || message.type === InteractionType.APPLICATION_COMMAND_AUTOCOMPLETE) {
-        console.log(message);
-        // getRandomCit(message);
+        const queryAttrs = {
+            // 'work_search%5Bwords_to%5D': 100
+        };
 
+        if (global.additionalTag) {
+            queryAttrs['work_search%5Bother_tag_names%5D'] = global.additionalTag;
+        }
+
+        const worksUrl = makeWorksUrl(global.seasonTag);
+        const { dom, randomWorkUrl } = await searchWorkPage(worksUrl, queryAttrs);
+
+        const { fandom, title, downloadLink, summary } = await getWorkData(dom);
+        const randomParagraphText = getRandomParagraph(dom).slice(0, 900);
+        const { media, otherLinks } = getWorkImages(dom);
+        const images = [];
+        media.map(el => {
+            el.map(img => {
+                images.push(img.media)
+            })
+        })
+
+        const embed = {
+            type: 'rich',
+            title: title,
+            fandom: fandom,
+            url: `${ao3Url}${randomWorkUrl}`,
+            fields: [
+                {
+                    name: 'Фандом',
+                    value: fandom,
+                },
+                {
+                    name: 'Ссылка для скачивания',
+                    value: `${ao3Url}${downloadLink}`,
+                },
+            ],
+        }
+        if (randomParagraphText) embed.fields.push({
+            name: 'Случайный абзац',
+            value: randomParagraphText
+        });
+        if (summary) embed.fields.push({
+            name: 'Саммари',
+            value: summary,
+        });
+        if (images.length) {
+            embed.image = { url: images[0] };
+            embed.fields.push({
+                name: 'Картинки',
+                value: images.join('\n'),
+            });
+        }
+        if (otherLinks.length) embed.fields.push({
+            name: 'Видео',
+            value: otherLinks.join('\n'),
+        });
+        res.status(200).send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+                embeds: [embed]
+            }
+        })
     } else {
         res.status(400).send({ error: "Unknown Type" });
     }
