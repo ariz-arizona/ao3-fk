0afd04ab03a6059e84571b50677435c079da2b86
arizona <ar.ariz.arizona@gmail.com>
2023-01-21 14:15:03 +0300

    additionalTag fix

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.listen(APP_PORT, () => {
     console.log(`listening on ${APP_PORT}`)
 });
 
-global.additionalTag = fkTagYears['2023'];
+global.additionalTag = fkTagYears['w2023'];
 global.seasonTag = winterFkTag;
